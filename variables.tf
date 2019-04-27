cbbef2e033fab3a4b599f9c265072853fc75b352
farrukh90 <farrukhsadykov@gmail.com>
2019-04-27 16:25:13 -0500

    added tags

--- variables.tf
+++ variables.tf
@@ -3,3 +3,6 @@ variable "bastion_max_size" {}
 variable "node_min_size" {}
 variable "node_max_size" {}
 variable "node_instance_type" {}
+variable "Env" {}
+variable "Created_by" {}
+variable "Dept" {}
